c76122b9a27a1b94f5911282ff68cee2f6ebdf17
Aleksandr <sasha___1943@mail.ru>
2018-08-21 22:56:08 +0300

    Login and registration

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,23 +1,55 @@
 import React from 'react';
 import '../index.css';
 import '../../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import axios from 'axios';
 
 class Login extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.signIn = this.signIn.bind(this);
+        this.handleEmailChange = this.handleEmailChange.bind(this);
+        this.handlePasswordChange = this.handlePasswordChange.bind(this);
+        this.state = {
+            email:'',
+            password:''
+        };
+    }
+
+    signIn(){
+        axios.post('/signin', {
+            email: this.state.email,
+            password: this.state.password
+        })
+            .then(function (response) {
+                console.log(response);
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+    }
+    handleEmailChange(e){
+        this.setState({email:e.target.value})
+    }
+    handlePasswordChange(e){
+        this.setState({password:e.target.value})
+    }
+
     render() {
         return <div className='container'>
                 <form className="form-signin text-center">
                     <h1 className="h3 mb-3 font-weight-normal">Please sign in</h1>
                     <label htmlFor="inputEmail" className="sr-only">Email address</label>
-                    <input type="email" id="inputEmail" className="form-control" placeholder="Email address" required
+                    <input type="email" onChange={this.handleEmailChange} id="inputEmail" className="form-control" placeholder="Email address" required
                            autoFocus/>
                     <label htmlFor="inputPassword" className="sr-only">Password</label>
-                    <input type="password" id="inputPassword" className="form-control" placeholder="Password" required/>
+                    <input type="password" onChange={this.handlePasswordChange} id="inputPassword" className="form-control" placeholder="Password" required/>
                     <div className="checkbox mb-3">
                         <label>
                             <input type="checkbox" value="remember-me"/> Remember me
                         </label>
                     </div>
-                    <button className="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+                    <button onClick={this.signIn} className="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
                 </form>
             </div>
     }
--- src/components/Registration.js
+++ src/components/Registration.js
@@ -2,9 +2,44 @@ import React from "react";
 import FieldRegistration from './FieldRegistration'
 import '../index.css';
 import '../../node_modules/bootstrap/dist/css/bootstrap.min.css';
-
+import axios from 'axios';
 
 class Registration extends React.Component {
+    constructor(props) {
+        super(props);
+        this.signUp = this.signUp.bind(this);
+        this.handleNameChange = this.handleNameChange.bind(this);
+        this.handleEmailChange = this.handleEmailChange.bind(this);
+        this.handlePasswordChange = this.handlePasswordChange.bind(this);
+        this.state = {
+            name:'',
+            email:'',
+            password:''
+        };
+    }
+    handleNameChange(e){
+        this.setState({name:e.target.value})
+    }
+    handleEmailChange(e){
+        this.setState({email:e.target.value})
+    }
+    handlePasswordChange(e){
+        this.setState({password:e.target.value})
+    }
+    signUp(){
+        axios.post('/signup', {
+            name: this.state.name,
+            email: this.state.email,
+            password: this.state.password
+        })
+            .then(function (response) {
+                console.log(response);
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+    }
+
     render() {
         return <div className='container'>
             <form>
@@ -15,7 +50,7 @@ class Registration extends React.Component {
                 <FieldRegistration name='Password' type='password' id='password'/>
                 <FieldRegistration name='Confirm password' type='password' id='confirm-password'/>
                 <div className='mb-3 text-center'>
-                    <button className="btn btn-lg btn-primary" type="submit">Create account</button>
+                    <button onClick={this.signUp} className="btn btn-lg btn-primary" type="submit">Create account</button>
                 </div>
             </form>
         </div>
