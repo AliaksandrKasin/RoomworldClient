6535a7690ca3ba8d6aa3864b22238cb3b5708709
sasha___1943 <sasha___1943@mail.ru>
2018-08-23 17:11:41 +0300

    axios

--- src/components/Login.js
+++ src/components/Login.js
@@ -3,44 +3,15 @@ import '../index.css';
 import '../../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import axios from 'axios';
 
+
 class Login extends React.Component {
 
     constructor(props) {
         super(props);
-        this.signIn = this.signIn.bind(this);
-        this.handleEmailChange = this.handleEmailChange.bind(this);
-        this.handlePasswordChange = this.handlePasswordChange.bind(this);
         this.state = {
-            email: '',
-            password: ''
+            email: 'adfasd',
+            password: 'asdfasdf'
         };
-    }
-
-    signIn() {
-        console.log(this.state.email);
-        console.log(this.state.password);
-
-        axios.post('https://localhost:5001/token', {
-                email: "sdfsdfsdfs",
-                password: "sdfsdfsdfs"
-            },{headers : {"Content-Type" : "application/x-www-form-urlencoded"}}
-        )
-            .then(function (response) {
-
-                console.log(response);
-
-            })
-            .catch(function (error) {
-                console.log(error);
-            });
-    }
-
-    handleEmailChange(e) {
-        this.setState({email: e.target.value})
-    }
-
-    handlePasswordChange(e) {
-        this.setState({password: e.target.value})
     }
 
     render() {
@@ -48,21 +19,36 @@ class Login extends React.Component {
             <form className="form-signin text-center">
                 <h1 className="h3 mb-3 font-weight-normal">Please sign in</h1>
                 <label htmlFor="inputEmail" className="sr-only">Email address</label>
-                <input type="email" id="inputEmail" onChange={this.handleEmailChange} className="form-control"
+                <input type="email" id="inputEmail" className="form-control"
                        placeholder="Email address" required
                        autoFocus/>
                 <label htmlFor="inputPassword" className="sr-only">Password</label>
-                <input type="password" id="inputPassword" onChange={this.handlePasswordChange} className="form-control"
+                <input type="password" id="inputPassword" className="form-control"
                        placeholder="Password" required/>
                 <div className="checkbox mb-3">
                     <label>
                         <input type="checkbox" value="remember-me"/> Remember me
                     </label>
                 </div>
-                <button onClick={this.signIn} className="btn btn-lg btn-primary btn-block" type="submit">Sign in
+                <button onClick={this.signIn} className="btn btn-lg btn-primary btn-block">Sign in
                 </button>
             </form>
         </div>
+    }
+
+
+    signIn() {
+        alert("asf");
+        axios.post('https://localhost:5001/token', {
+            email: 'sdgfs@sd',
+            password: 'sdfsdsdf'
+        })
+            .then(function (response) {
+                console.log(response);
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
     }
 }
 
