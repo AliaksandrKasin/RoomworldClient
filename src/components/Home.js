0cb03d645e0c354f0c8cb2c09321ea68c10aac0c
sasha___1943 <sasha___1943@mail.ru>
2018-09-05 16:19:26 +0300

    css

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,30 @@
 import React from "react";
 import AlbomCardFlat from "./AlbomCardFlat";
 
+
 class Home extends React.Component {
     render() {
-        return <div>
-            <AlbomCardFlat/>
+        return <div className="find">
+
+            <div className="row">
+                <div className="col-md-2 ">
+                    <input type="text" className="form-control h1 search" placeholder="Where do you want to go?"/>
+                </div>
+                <div className="col-md-1">
+                    <input type="text" className="form-control search" placeholder="From"/>
+                </div>
+                <div className="col-md-1">
+                    <input type="text" className="form-control search" placeholder="To"/>
+                </div>
+                <div className="col-md-1">
+                    <button className="btn btn-secondary btn-primary search" type='button'>Search
+                    </button>
+
+                </div>
+
+
+            </div>
+
         </div>
 
     }
