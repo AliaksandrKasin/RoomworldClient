aad434c5c15113f489d3ee6fae1fcd89698b6c85
Aleksandr <sasha___1943@mail.ru>
2018-09-27 21:45:38 +0300

    map

--- src/components/map.js
+++ src/components/map.js
@@ -1,29 +1,46 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import GoogleMapReact from 'google-map-react';
+import Geocode from "react-geocode";
 
-const AnyReactComponent = ({ text }) => <div>{text}</div>;
+const AnyReactComponent = ({text}) => <div>
+    <img className="button_cursor_pointer" onClick={()=>alert("asdasdasa")} className="img_size_4"
+         src="https://cdn4.iconfinder.com/data/icons/iconsimple-places/512/pin_2-512.png"/>
+    <h6>{text}</h6>
+</div>;
 
 class Map extends Component {
     static defaultProps = {
         center: {
-            lat: 59.95,
-            lng: 30.33
+            lat: 48.85837009999999,
+            lng: 2.2944813
         },
         zoom: 11
     };
 
     render() {
+        Geocode.setApiKey("AIzaSyCNmZiicfeXMG-PG4HQNU4lzX4OB-ci-NY");
+        Geocode.enableDebug();
+        Geocode.fromAddress("Eiffel Tower").then(
+            response => {
+                const {lat, lng} = response.results[0].geometry.location;
+                console.log(lat, lng);
+            },
+            error => {
+                console.error(error);
+            }
+        );
+
         return (
             <div className="map">
                 <GoogleMapReact
                     defaultCenter={this.props.center}
                     defaultZoom={this.props.zoom}
-                    bootstrapURLKeys={{ key: "AIzaSyDup-Ic_V4ILMytmg8to5a8tohSkzmLbus" }}
+                    bootstrapURLKeys={{key: "AIzaSyCNmZiicfeXMG-PG4HQNU4lzX4OB-ci-NY"}}
                 >
                     <AnyReactComponent
-                        lat={59.955413}
-                        lng={30.337844}
-                        text={''}
+                        lat={48.85837009999999}
+                        lng={2.2944813}
+                        text={'Efel Tower'}
                     />
                 </GoogleMapReact>
             </div>
