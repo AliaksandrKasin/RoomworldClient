4078fdb45749606faf29c3d8c81262ed4345b3b8
sasha___1943 <sasha___1943@mail.ru>
2018-11-30 18:01:37 +0300

    house filter

--- src/components/houseFilter/counterAmenitiesSmall.js
+++ src/components/houseFilter/counterAmenitiesSmall.js
@@ -11,7 +11,7 @@ class CounterAmenitiesSmall extends React.Component {
 
     render() {
         return <div className="d-flex mt-3">
-            <div className="counter-content mr-3">
+            <div className="counter-content mr-1">
                 {(this.state.counter === 0) ? <span className="counter-amount">Any</span> :
                     <span className="counter-amount">{this.state.counter}</span>}
                 <span className="counter-title">{this.props.title}</span>
@@ -19,8 +19,12 @@ class CounterAmenitiesSmall extends React.Component {
 
             <div className="d-flex justify-content-end">
                 <div className="counter-button"
-                     onClick={() => (this.state.counter > 0) && this.setState({counter: this.state.counter - 1})}></div>
-                <div className="counter-button" onClick={() => this.setState({counter: this.state.counter + 1})}></div>
+                     onClick={() => (this.state.counter > 0) && this.setState({counter: this.state.counter - 1})}>
+                    <i className="fas fa-minus counter-button-icon"></i>
+                </div>
+                <div className="counter-button" onClick={() => this.setState({counter: this.state.counter + 1})}>
+                    <i className="fas fa-plus counter-button-icon"></i>
+                </div>
             </div>
         </div>
     }
